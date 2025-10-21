4bb026d601931b8c58e2ab0192e2b5022ff3ec75
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2025-10-21 18:02:47 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,25 @@ const jetbrainsMono = JetBrains_Mono({
 })
 
 export const metadata: Metadata = {
-  title: "TechAccountingPro | Advanced Accounting Technology Platform",
-  description:
-    "Access expert insights, actionable templates, and proven methodologies to modernize your accounting practices with cutting-edge technology.",
-    generator: 'v0.app'
+  title: 'TechAccountingPro | Expert Technical Accounting Services Tailored to Your Business Needs.',
+  description: 'Solve complex crypto accounting questions with TechAccountingPro. Access expert insights, actionable templates, and proven methodologies to modernize your accounting practices with cutting-edge technology. ',
+  openGraph: {
+    url: 'https://techaccountingpro.com/',
+    title: 'TechAccountingPro | Your Technical Accounting Expert.',
+    description: 'Solve complex crypto accounting questions with TechAccountingPro.',
+    images: ['/placeholder-logo.png'],
+    siteName: 'TechAccountingPro',
+    locale: 'en_US',
+    type: 'website'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@tech_accounting',
+    creator: '@tech_accounting',
+    title: 'TechAccountingPro | Expert Technical Accounting Services Tailored to Your Business Needs.',
+    description: 'Unlock Complex Accounting Solutions with TechAccountingPro.',
+    images: ['/placeholder-logo.png']
+  }
 }
 
 export default function RootLayout({
