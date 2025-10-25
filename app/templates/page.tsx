dcfffb1f2af199308283c9ed5302b0662f414224
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2025-10-25 14:04:02 -0400

    Update page.tsx

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -7,13 +7,12 @@ import { TemplatesPageSkeleton } from "@/components/templates-page-skeleton"
 export default function TemplatesPage() {
   return (
     <div className="min-h-screen bg-white flex flex-col">
-      <SiteHeader />
 
       <Suspense fallback={<TemplatesPageSkeleton />}>
         <TemplatesPageContent />
       </Suspense>
 
-      <SiteFooter />
+    
     </div>
   )
 }
